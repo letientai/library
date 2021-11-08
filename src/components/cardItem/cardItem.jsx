f17c666b66f984c34e025f70a589ec6727c8752f
Tài Lê <letai97md@gmail.com>
2021-11-08 20:43:38 +0700

    handle form-comment and carousel

--- src/components/cardItem/cardItem.jsx
+++ src/components/cardItem/cardItem.jsx
@@ -1,12 +1,36 @@
 import React from "react";
-import './cardItem.scss';
+import "./cardItem.scss";
+import Rating from "@mui/material/Rating";
+import { useHistory } from "react-router-dom";
 
-const CardItem = (props) =>{
-    const item = props.product
-    return(
-        <div className="cardItem-container">
-            <p>{item.name}</p>
+const CardItem = (props) => {
+  const item = props.product;
+  const history = useHistory();
+
+  const moveToDetail = () => {
+    history.push(`/product/${item.id}`);
+  };
+
+  return (
+    <div className="cardItem-container" onClick={moveToDetail}>
+      <div className="image">
+        <img src={item.image} alt="" />
+      </div>
+      <h5>{item.name}</h5>
+      <Rating
+        name="half-rating-read"
+        defaultValue={item.rating}
+        precision={0.5}
+        readOnly
+        className="start"
+      />
+      <div className="price">
+        <span>{item.price} VNĐ</span>
+        <div className="deal">
+          <p>-20%</p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default CardItem;
